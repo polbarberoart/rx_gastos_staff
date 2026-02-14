1958852b3f142b59c607d84fa7bc8600930ad6b7
Pol Barbero <pbarbero@seidor.es>
2026-02-14 16:28:41 +0100

    WELCOME WILLY

--- gastos_app/gastos_app.py
+++ gastos_app/gastos_app.py
@@ -14,7 +14,7 @@ def index() -> rx.Component:
     return rx.container(
         rx.color_mode.button(position="top-right"),
         rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
+            rx.heading("Welcome WILLY!", size="9"),
             rx.text(
                 "Get started by editing ",
                 rx.code(f"{config.app_name}/{config.app_name}.py"),
