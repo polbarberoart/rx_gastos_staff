fdce0cd69f0d90997a09308f059f206fd20b1bab
Pol Barbero <pbarbero@seidor.es>
2026-02-14 21:14:20 +0100

    test github actions

--- rxconfig.py
+++ rxconfig.py
@@ -3,6 +3,7 @@ import reflex as rx
 config = rx.Config(
     app_name="gastos_app",
     api_url="https://rxgastosstaff-production.up.railway.app", 
+    cors_allowed_origins="https://rx-gastos-staff.vercel.app",
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
