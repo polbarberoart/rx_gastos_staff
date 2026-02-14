ed5f5eff4be7d82505ebdfd2227298d20084fc2c
Pol Barbero <pbarbero@seidor.es>
2026-02-14 17:45:48 +0100

    ajsutes api config

--- build.ps1
+++ build.ps1
@@ -2,8 +2,7 @@
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 python -m reflex init
-# Así se pasan variables de entorno en PowerShell antes de un comando:
-$env:API_URL="https://rxgastosstaff-production.up.railway.app/"; python -m reflex export --frontend-only
+python -m reflex export --frontend-only
 Expand-Archive -Path frontend.zip -DestinationPath public -Force
 Remove-Item -Path frontend.zip -Force
 deactivate
--- rxconfig.py
+++ rxconfig.py
@@ -2,6 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="gastos_app",
+    api_url="https://rxgastosstaff-production.up.railway.app", 
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
