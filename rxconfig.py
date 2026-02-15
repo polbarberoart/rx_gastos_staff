5edde48b7ccd5f6710be007d601baf711be32cd9
Pol Barbero <pbarbero@seidor.es>
2026-02-15 09:04:42 +0100

    cors bug

--- rxconfig.py
+++ rxconfig.py
@@ -3,7 +3,10 @@ import reflex as rx
 config = rx.Config(
     app_name="gastos_app",
     api_url="https://rxgastosstaff-production.up.railway.app", 
-    cors_allowed_origins="https://rx-gastos-staff.vercel.app",
+    cors_allowed_origins=[
+        "https://rx-gastos-staff.vercel.app",
+        "http://localhost:3000"
+    ],
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
